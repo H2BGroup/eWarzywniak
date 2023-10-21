1c464f3ffce91d35ec4c9d4eebeb12bf4c9fb468
JJayJohnny <jasiek.barczewski@gmail.com>
2023-10-21 15:16:43 +0200

    basic product add function

--- initialize/main.py
+++ initialize/main.py
@@ -2,8 +2,8 @@ from prestapyt import PrestaShopWebServiceDict
 from pprint import pprint
 import json
 
-def getParentCategoryId(prestashop, parent):
-    ids = prestashop.search('categories', options={'filter[name]': parent})
+def getCategoryId(prestashop, category):
+    ids = prestashop.search('categories', options={'filter[name]': category})
     if len(ids) > 0:
         return ids[0]
     else:
@@ -27,6 +27,28 @@ def addCategory(prestashop, categoryName, parentId):
 
     return response['prestashop']['category']['id']
 
+def addProduct(prestashop, product, categoryId):
+    schema = prestashop.get('products', options={'schema': 'synopsis'})
+
+    schema['product'].pop('position_in_category')
+    schema['product'].pop('manufacturer_name')
+    schema['product'].pop('quantity')
+
+    schema['product']['state'] = 1
+    schema['product']['active'] = 1
+    schema['product']['description']['language'][0]['value'] = '\n'.join(product['description'])
+    schema['product']['description_short']['language'][0]['value'] = product['short_description']
+
+    schema['product']['id_category_default']['value'] = categoryId
+    schema['product']['name']['language'][0]['value'] = product['title']
+    schema['product']['price']['value'] = float(product['price'].replace(',', '.'))
+    schema['product']['show_price']['value'] = 1
+    
+
+    pprint(schema)
+
+    return prestashop.add('products', schema)
+
 
 WEBSERVICE_KEY = 'IK9V9749QYEE5V1QIASHG729V7YJQLEH'
 
@@ -37,12 +59,18 @@ categories = {}
 f = open('../scraper/warzywniak.json')
 data = json.load(f)
 
-parentCategoryId = getParentCategoryId(prestashop, "Kategorie")
-for entry in data:
-    if 'category_name' in entry:
-        id = addCategory(prestashop, entry['category_name'], parentCategoryId)
-        categories[entry['category_name']] = id
-    else:
-        pass
+parentCategoryId = getCategoryId(prestashop, "Kategorie")
 
-pprint(categories)
+if parentCategoryId is None:
+    parentCategoryId = addCategory(prestashop, "Kategorie", 2)
+
+# for entry in data:
+#     if 'category_name' in entry:
+#         id = addCategory(prestashop, entry['category_name'], parentCategoryId)
+#         categories[entry['category_name']] = id
+#     else:
+#         pass
+
+product = {"title": "Ekologiczna kostka rosołowa wołowa", "category": ["Przyprawy, sól, cukier"], "price": "6,49", "image": "https://skladwarzywiowocow.pl/wp-content/uploads/2022/11/ekologiczna-kostka-rosolowa-wolowa-416x416.png", "large_image": "https://skladwarzywiowocow.pl/wp-content/uploads/2022/11/ekologiczna-kostka-rosolowa-wolowa.png", "short_description": "Kostki rosołowe to kompozycja ekologicznych ziół i przypraw polecana do przyrządzania zup, rosołów, sosów, makaronów i innych potraw. Kostki nie zawierają glutaminianu sodu czy oleju palmowego. Z ich pomocą łatwo przygotujesz zdrową zupę dla całej rodziny.", "description": ["Opis", "Skład:", "\nsól morska, masło shea*, pomidory*, skrobia kukurydziana*, prażona cebula*, seler*, 2,5% sproszkowane mięso wołowe*, skarmelizowany cukier*, kurkuma*, ekstrakt z drożdży,  naturalny aromat, lubczyk liście*, czosnek*, pieprz*.", "\n* produkty pochodzące z kontrolowanych, certyfikowanych upraw ekologicznych"]}
+
+pprint(addProduct(prestashop, product, 3))
